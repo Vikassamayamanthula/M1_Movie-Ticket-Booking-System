7405255169d3e4c3608763bed1a88b74ff1c832d
Vikassamayamanthula <100985462+Vikassamayamanthula@users.noreply.github.com>
2022-03-31 22:12:23 +0530

    Update end.h

--- 3_Implementation/end.h
+++ 3_Implementation/end.h
@@ -2,6 +2,6 @@ void end(void){
  system("cls");
  printf("\t\t\tTHANK YOU FOR USING OUR SERVICES\n");
  printf("\t\t\t\t  Visit Us At \n\n");
- printf("\t\t\t    vendingmachine.com.\n\n");
+ printf("\t\t\t    movie ticket booking system.\n\n");
  printf(" Programmed by : S.Vikas \n");
 } 
