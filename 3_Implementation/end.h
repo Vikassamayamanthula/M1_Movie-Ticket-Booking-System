bfa94700e21812648e399cf132726968471f9c77
Vikassamayamanthula <100985462+Vikassamayamanthula@users.noreply.github.com>
2022-03-31 22:14:22 +0530

    Delete end.h

--- 3_Implementation/end.h
+++
@@ -1,7 +0,0 @@
-void end(void){
- system("cls");
- printf("\t\t\tTHANK YOU FOR USING OUR SERVICES\n");
- printf("\t\t\t\t  Visit Us At \n\n");
- printf("\t\t\t    movie ticket booking system.\n\n");
- printf(" Programmed by : S.Vikas \n");
-} 
